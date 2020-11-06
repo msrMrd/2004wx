4ddd44c4c985d01f5e249edad9db6eb9f28d1291
xiaoshu <2140395110@qq.com>
2020-11-06 16:28:38 +0800

    测试

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Test;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
+use Log;
 use Illuminate\Support\Facades\Redis;
 
 class TestController extends Controller
